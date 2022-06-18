5be4ff274dff062929ee6f1f72aef6ca2e4701da
Romina Martin <romina.martin@hotjar.com>
2022-06-18 13:21:15 +0200

    feat: attempt on cache

--- next.config.js
+++ next.config.js
@@ -1,11 +1,41 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa');
+const { PHASE_PRODUCTION_BUILD } = require('next/constants');
 
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-  },
-  reactStrictMode: true,
-  styledComponents: true,
-});
+module.exports = (phase, { defaultConfig }) => {
+  const config = {
+    ...defaultConfig,
+    reactStrictMode: true,
+    styledComponents: true,
+    pwa: {
+      dest: 'public',
+      dynamicStartUrl: false, // precache home page instead of storing it in runtime cache by default
+      register: false,
+      skipWaiting: false,
+      buildExcludes: [/middleware-manifest\.json$/], // for Next 12, see https://github.com/shadowwalker/next-pwa/issues/288
+    },
+  };
+
+  if (phase === PHASE_PRODUCTION_BUILD) {
+    // Attributes generateBuildId and additionalManifestEntries are only needed
+    // for the build and calculating their value is time-consuming.
+    // So we add them here, just for the build.
+    const getBuildId = require('./utils/buildid.js');
+    const getStaticPrecacheEntries = require('./utils/staticprecache.js');
+    const getGeneratedPrecacheEntries = require('./utils/precache.js');
+
+    const buildId = getBuildId();
+
+    config.generateBuildId = getBuildId;
+    config.pwa.additionalManifestEntries = [
+      ...getStaticPrecacheEntries({
+        // exclude icon-related files from the precache since they are platform specific
+        // note: no need to pass publicExcludes to next-pwa, it's not used for anything else
+        publicExcludes: ['!*.png', '!*.ico', '!browserconfig.xml'],
+      }),
+      ...getGeneratedPrecacheEntries(buildId),
+    ];
+  }
+
+  return withPWA(config);
+};
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,7 +13,18 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <meta name="application-name" content="Teacher Canaria" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+          <meta name="apple-mobile-web-app-title" content="Teacher Canaria" />
+          <meta name="description" content="Bike progress" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="theme-color" content="#000000" />
+
+          <link rel="apple-touch-icon" href="/images/apple-touch-icon.png" />
+
           <link rel="manifest" href="/manifest.json" />
+
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400&display=swap"
             rel="stylesheet"
